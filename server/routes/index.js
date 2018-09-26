0da8f20b952b36a5f21abe74fb6cfd37c320ec25
freshcoderchan <freshcoderjack@outlook.com>
2018-09-26 07:22:59 -0700

    Serialize & Deserialize & Add 'login' Route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.send('Go back and register!');
+});
+
+
 module.exports = router;
